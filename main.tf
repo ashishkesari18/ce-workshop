54900e83365c612abb7e6a19c687590d729629cb
Ashish Kesari <88103934+ashishkesari18@users.noreply.github.com>
2026-06-29 14:57:52 -0500

    Add backend and module reference

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   region  = "us-east-2"
   profile = "CE"
 }
@@ -6,6 +6,27 @@ provider "aws" {
 module "s3_bucket" {
   source = "github.com/ashishkesari18/terraform-modules//modules/s3"
 
-  bucket_name = "devops-central-us-east-2-new"
+  bucket_name = "devops-central-us-east-2-news"
   environment = "Dev"
+}*/
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "CE"
 }
+locals {
+    buckets = {
+        "bucket1" = "devops-central-us-east-2-new"
+        "bucket2" = "devops-central-us-east-2-new-again"
+        "bucket3" = "devops-central-us-east-2-bucket3"
+        "bucket4" = "devops-central-us-east-2-bucket4"
+        "bucket5" = "devops-central-us-east-2-bucket5"
+    }
+}
+
+module "s3_bucket" {
+  source = "github.com/ashishkesari18/terraform-modules//modules/s3"
+  for_each = local.buckets
+  bucket_name = each.value
+  environment = "Dev"
+  }
